9c78ce8d4648202e702ab85ae0e410df81c3cd9d
Aaron Lin <aaronlin0516@hotmail.com>
2019-03-20 16:30:18 +0800

    change to while !=0

--- pw.py
+++ pw.py
@@ -1,12 +1,14 @@
 password = 'a123456'
 i=3
-while True:
+while i != 0:
 	s1 = input('請輸入密碼: ')
 	if s1 == password:
 		print('登入成功!')
 		break #成功之後離開迴圈
 	else:
 		i = i -1
-		print('密碼錯誤! 還有', i, '次機會')
 		if i == 0:
+			print('失敗三次，強制登出')
 			break
+		print('密碼錯誤! 還有', i, '次機會')
+		
